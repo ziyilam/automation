f8bdb886faf73abbe1961a8b7cd86a8ebdfb2e42
ZY Lam <zylam@iconixconsulting.com>
2017-08-13 23:36:27 +0800

    house keeping constants.java and log4j2.xml

--- src/config/Constants.java
+++ src/config/Constants.java
@@ -6,24 +6,24 @@ public class Constants {
 	//public static final String Chrome_Property2 = ".\\Browser-Driver\\chromedriver.exe";
 	//for MAC
 	public static final String Chrome_Property2 = ".//Browser-Driver//chromedriver";
+	
 	public static final String Firefox_Property1 = "webdriver.gecko.driver";
 	//for PC
 	//public static final String Firefox_Property2 = ".\\Browser-Driver\\geckodriver.exe";
 	//for MAC
 	public static final String Firefox_Property2 = ".//Browser-Driver//geckodriver";
+	
 	public static final String IE_Property1 = "webdriver.ie.driver";
 	//for PC
 	public static final String IE_Property2 = ".\\Browser-Driver\\IEDriverServer.exe";
 	// No IEDriverServers for MACs
 	
-	//public static final String Path_TestData = "C:\\Users\\zy.lam\\eclipse-workspace\\Hybrid Keyword Driven\\src\\dataEngine\\DataEngine.xlsx";
 	//for PC
 	//public static final String Path_TestData = ".\\src\\dataEngine\\DataEngine.xlsx";
 	//for MAC
 	public static final String Path_TestData = ".//src//dataEngine//DataEngine.xlsx";
-	//public static final String Path_TestData = "..\\scr\\dataEngine\\DataEngine.xlsx";
 	public static final String File_TestData = "DataEngine.xlsx";
-	//public static final String Path_log4j = ".\\src\\xml\\log4j.xml";
+	
 
 	public static final int Col_TestCaseID = 0;
 	public static final int Col_TestStepID = 1;
