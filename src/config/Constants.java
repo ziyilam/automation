3ade4851caa7b4444a1d10eebf63330d1333dc2b
Lam Zi Yi <zy.lam@vodka.local>
2017-08-16 11:35:47 +0800

    house keeping constants for mac and pc

--- src/config/Constants.java
+++ src/config/Constants.java
@@ -2,28 +2,19 @@ package config;
 
 public class Constants {
 	public static final String Chrome_Property1 = "webdriver.chrome.driver";
-	//for PC
-	//public static final String Chrome_Property2 = ".\\Browser-Driver\\chromedriver.exe";
-	//for MAC
-	public static final String Chrome_Property2 = ".//Browser-Driver//chromedriver";
-	
 	public static final String Firefox_Property1 = "webdriver.gecko.driver";
-	//for PC
-	//public static final String Firefox_Property2 = ".\\Browser-Driver\\geckodriver.exe";
-	//for MAC
-	public static final String Firefox_Property2 = ".//Browser-Driver//geckodriver";
-	
 	public static final String IE_Property1 = "webdriver.ie.driver";
-	//for PC
-	public static final String IE_Property2 = ".\\Browser-Driver\\IEDriverServer.exe";
-	// No IEDriverServers for MACs ////
-	
-	//for PC
-	//public static final String Path_TestData = ".\\src\\dataEngine\\DataEngine.xlsx";
-	//for MAC
-	public static final String Path_TestData = ".//src//dataEngine//DataEngine.xlsx";
 	public static final String File_TestData = "DataEngine.xlsx";
-	
+	//for PC
+	public static final String Chrome_Property2 = ".\\Browser-Driver\\chromedriver.exe";
+	public static final String Firefox_Property2 = ".\\Browser-Driver\\geckodriver.exe";
+	public static final String IE_Property2 = ".\\Browser-Driver\\IEDriverServer.exe";
+	public static final String Path_TestData = ".\\src\\dataEngine\\DataEngine.xlsx";
+	//for MAC
+	//public static final String Chrome_Property2 = ".//Browser-Driver//chromedriver";
+	//public static final String Firefox_Property2 = ".//Browser-Driver//geckodriver";
+	// No IEDriverServers for MACs ////
+	//public static final String Path_TestData = ".//src//dataEngine//DataEngine.xlsx";
 
 	public static final int Col_TestCaseID = 0;
 	public static final int Col_TestStepID = 1;
@@ -32,9 +23,7 @@ public class Constants {
 	public static final int Col_TestData = 5;
 	public static final int Col_CaseResults = 3;
 	public static final int Col_StepResults = 6;
-	
 	public static final int Col_RunMode = 2;
-
 	public static final String Sheet_TestCases = "TestCases";
 	public static final String Sheet_TestSteps = "TestSteps";
 	public static final String Sheet_TestData = "TestData";
